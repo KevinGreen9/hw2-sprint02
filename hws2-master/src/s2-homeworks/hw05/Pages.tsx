f0f83de1db7e55a62b67d3e4528fd6aece1e3010
IvcencoSemion <cema.56.s@mail.ru>
2026-08-06 19:46:54 +0100

    add routes and route

--- hws2-master/src/s2-homeworks/hw05/Pages.tsx
+++ hws2-master/src/s2-homeworks/hw05/Pages.tsx
@@ -4,6 +4,7 @@ import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
+import {Home} from "@react-zero-ui/icon-sprite";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -18,15 +19,14 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                <Route path='/' element={<Navigate to={'/pre-junior'}/>}/>
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path='/pre-junior' element={<PreJunior/>}/>
+                <Route path='/junior' element={<Junior/>}/>
+                <Route path='/junior-plus' element={<JuniorPlus/>}/>
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path='/error404' element={<Error404/>}/>
+                <Route path='/*' element={<Navigate to={'/error404'}/>}/>
             </Routes>
         </div>
     )
